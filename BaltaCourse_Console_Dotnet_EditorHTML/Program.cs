608d385489b7534ba2c14f283d746d72d4bde7f3
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-01-09 23:02:25 -0300

    EditorHTML: Criação projeto

---
+++ BaltaCourse_Console_Dotnet_EditorHTML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaltaCourse_Console_Dotnet_EditorHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
